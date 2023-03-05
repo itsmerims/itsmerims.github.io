eaaed24650c5bf73282ab15e81df566be325b5e1
itsmerims <johnrimuelbigata@gmail.com>
2023-03-05 22:04:17 +0800

    modified:   components/ProjectSection.tsx

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -84,7 +84,7 @@ const ProjectSection = () => {
       {showModal ? (
         <>
           <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            className="justify-center items-center w-11/12 mx-auto flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
